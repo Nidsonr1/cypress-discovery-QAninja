db8da1837daef8a604e5178456a92cf513c7258e
Nidsonr1 <nidsoncampos@hotmail.com>
2022-01-17 12:38:06 -0300

    - Refatorando a massa de testes para ingles - Cenario de teste com CPF incorreto

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -10,44 +10,44 @@ describe('Cadastro', () => {
     cy.get("#page-deliver form h1")
       .should('have.text', 'Cadastre-se para  fazer entregas');
 
-    var entregador = {
-      nome: 'John Doe',
+    var deliver = {
+      name: 'John Doe',
       cpf: '04864870322',
-      email: 'entregador.teste@gmail.com',
+      email: 'deliver.teste@gmail.com',
       whatsapp: '(88) 3839-6986',
-      endereco: {
-        cep: '63021-240',
-        rua: 'Rua Antônio Eliomar Félix',
-        numero: '639',
-        complemento: 'Apt 142',
-        bairro: 'Aeroporto',
-        cidade_uf: 'Juazeiro do Norte/CE'
+      address: {
+        postalcode: '63021-240',
+        street: 'Rua Antônio Eliomar Félix',
+        number: '639',
+        details: 'Apt 142',
+        district: 'Aeroporto',
+        city_state: 'Juazeiro do Norte/CE'
       },
-      metodo_entrega: 'Moto',
+      deliver_method: 'Moto',
       cnh: 'cnh-digital.jpg'
     }
 
     //Preenche campos Dados 
-    cy.get('input[name="name"]').type(entregador.nome);
-    cy.get('input[name="cpf"]').type(entregador.cpf);
-    cy.get('input[name="email"]').type(entregador.email);
-    cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
+    cy.get('input[name="name"]').type(deliver.name);
+    cy.get('input[name="cpf"]').type(deliver.cpf);
+    cy.get('input[name="email"]').type(deliver.email);
+    cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
     
     //Preenche campos de Endereço
-    cy.get('input[name="postalcode"]').type(entregador.endereco.cep);
+    cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
     cy.get('input[type=button][value="Buscar CEP"]').click();
 
-    cy.get('input[name="address"]').should('have.value', entregador.endereco.rua);
+    cy.get('input[name="address"]').should('have.value', deliver.address.street);
 
-    cy.get('input[name="address-number"]').type(entregador.endereco.numero);
-    cy.get('input[name="address-details"]').type(entregador.endereco.complemento);
+    cy.get('input[name="address-number"]').type(deliver.address.number);
+    cy.get('input[name="address-details"]').type(deliver.address.details);
 
-    cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro);
-    cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+    cy.get('input[name="district"]').should('have.value', deliver.address.district);
+    cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
 
-    cy.contains('.delivery-method li', entregador.metodo_entrega).click();
+    cy.contains('.delivery-method li', deliver.deliver_method).click();
     
-    cy.get('input[accept^="image"]').attachFile(`/images/${entregador.cnh}`);
+    cy.get('input[accept^="image"]').attachFile(`/images/${deliver.cnh}`);
 
     cy.get('form button[type="submit"]').click();
 
@@ -57,5 +57,60 @@ describe('Cadastro', () => {
       .should('have.text', expectedMessageSwal);
 
     cy.get('button[class="swal2-confirm swal2-styled"]').click();
+  });
+
+  it('CPF incorreto', () => {
+    cy.viewport(1440, 900);
+    cy.visit('https://buger-eats.vercel.app');
+
+    cy.get('a[href="/deliver"]')
+      .click();
+
+    cy.url('https://buger-eats.vercel.app').should('match', /deliver/);
+    cy.get("#page-deliver form h1")
+      .should('have.text', 'Cadastre-se para  fazer entregas');
+
+    var deliver = {
+      name: 'John Doe',
+      cpf: '04864870322AA',
+      email: 'deliver.teste@gmail.com',
+      whatsapp: '(88) 3839-6986',
+      address: {
+        postalcode: '63021-240',
+        street: 'Rua Antônio Eliomar Félix',
+        number: '639',
+        details: 'Apt 142',
+        district: 'Aeroporto',
+        city_state: 'Juazeiro do Norte/CE'
+      },
+      deliver_method: 'Moto',
+      cnh: 'cnh-digital.jpg'
+    }
+
+    //Preenche campos Dados 
+    cy.get('input[name="name"]').type(deliver.name);
+    cy.get('input[name="cpf"]').type(deliver.cpf);
+    cy.get('input[name="email"]').type(deliver.email);
+    cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+    
+    //Preenche campos de Endereço
+    cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
+    cy.get('input[type=button][value="Buscar CEP"]').click();
+
+    cy.get('input[name="address"]').should('have.value', deliver.address.street);
+
+    cy.get('input[name="address-number"]').type(deliver.address.number);
+    cy.get('input[name="address-details"]').type(deliver.address.details);
+
+    cy.get('input[name="district"]').should('have.value', deliver.address.district);
+    cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
+
+    cy.contains('.delivery-method li', deliver.deliver_method).click();
+    
+    cy.get('input[accept^="image"]').attachFile(`/images/${deliver.cnh}`);
+
+    cy.get('form button[type="submit"]').click();
+
+    cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
   });
 });
