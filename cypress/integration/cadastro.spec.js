dd220da091bdc329c2849fb0848b010c307dc14a
Nidsonr1 <nidsoncampos@hotmail.com>
2022-01-19 14:00:34 -0300

    Utilizando Fixtures

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,52 +1,24 @@
 import signup from '../pages/SignupPage'
 
 describe('Cadastro', () => {
+  beforeEach(function() {
+    cy.fixture("deliver").then((d) => {
+      this.deliver = d;
+    })
+  })
 
-  it('Usuário deve se tornar um entregador', () => {
-    var deliver = {
-      name: 'John Doe',
-      cpf: '04864870322',
-      email: 'deliver.teste@gmail.com',
-      whatsapp: '(88) 3839-6986',
-      address: {
-        postalcode: '63021-240',
-        street: 'Rua Antônio Eliomar Félix',
-        number: '639',
-        details: 'Apt 142',
-        district: 'Aeroporto',
-        city_state: 'Juazeiro do Norte/CE'
-      },
-      deliver_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
+  it('Usuário deve se tornar um entregador', function() {
     const expectedMessageSwal = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
 
     signup.go();
-    signup.fillForm(deliver);
+    signup.fillForm(this.deliver.signup);
     signup.submit()
     signup.modalContentShouldBe(expectedMessageSwal)
   });
 
-  it('CPF incorreto', () => {
-    var deliver = {
-      name: 'John Doe',
-      cpf: '04864870322AA',
-      email: 'deliver.teste@gmail.com',
-      whatsapp: '(88) 3839-6986',
-      address: {
-        postalcode: '63021-240',
-        street: 'Rua Antônio Eliomar Félix',
-        number: '639',
-        details: 'Apt 142',
-        district: 'Aeroporto',
-        city_state: 'Juazeiro do Norte/CE'
-      },
-      deliver_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
-
+  it('CPF incorreto', function() {
     signup.go();
-    signup.fillForm(deliver);
+    signup.fillForm(this.deliver.cpf_inv);
     signup.submit();
     signup.alertMessageShouldBe('Oops! CPF inválido');
   });
