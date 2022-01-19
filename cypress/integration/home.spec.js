f2e36496bac36e20030f87912b75c204a0a1e748
Nidsonr1 <nidsoncampos@hotmail.com>
2022-01-19 14:09:57 -0300

    Ajustes para Ambiente de Teste - Alteraçao de rota - Alteraçao do localizador do nome do entregador

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 describe("home page", () => {
   it("App deve estar online", () => {
     cy.viewport(1440, 900); //Ajusta a resolução do navegador
-    cy.visit("https://buger-eats.vercel.app");
+    cy.visit("/");
     cy.get('#page-home h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
   });
 });
