f3ba0444ef91d3211e62db0632bd16ec96bd039a
Nidsonr1 <nidsoncampos@hotmail.com>
2022-01-20 09:41:54 -0300

    Utilizando contexto para testar campos obrigatorios

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -2,12 +2,6 @@ import signupPage from '../pages/SignupPage'
 import signupFactory from '../factories/SignupFactory';
 
 describe('Signup', () => {
-  // beforeEach(function() {
-  //   cy.fixture("deliver").then((d) => {
-  //     this.deliver = d;
-  //   })
-  // })
-  
   it('User should be deliverboy', function() {
     const expectedMessageSwal = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
 
@@ -39,15 +33,27 @@ describe('Signup', () => {
     signupPage.alertMessageShouldBe('Oops! Email com formato inválido.');
   });
 
-  it('Required fields', function() {
-    signupPage.go();
-    signupPage.submit();
-    signupPage.alertMessageShouldBe('É necessário informar o nome');
-    signupPage.alertMessageShouldBe('É necessário informar o CPF');
-    signupPage.alertMessageShouldBe('É necessário informar o email');
-    signupPage.alertMessageShouldBe('É necessário informar o CEP');
-    signupPage.alertMessageShouldBe('É necessário informar o número do endereço');
-    signupPage.alertMessageShouldBe('Selecione o método de entrega');
-    signupPage.alertMessageShouldBe('Adicione uma foto da sua CNH');
-  })
+  context('Required fields', function() {
+
+    const messages = [
+      {field: 'name', output: 'É necessário informar o nome'},
+      {field: 'cpf', output: 'É necessário informar o CPF'},
+      {field: 'email', output: 'É necessário informar o email'},
+      {field: 'postalcode', output: 'É necessário informar o CEP'},
+      {field: 'number', output: 'É necessário informar o número do endereço'},
+      {field: 'delivery_method', output: 'Selecione o método de entrega'},
+      {field: 'cnh', output: 'Adicione uma foto da sua CNH'}
+    ]
+
+    before(function() {
+      signupPage.go();
+      signupPage.submit();
+    });
+
+    messages.forEach(function(message) {
+      it(`${message.field} is required`, function() {
+        signupPage.alertMessageShouldBe(message.output);
+      });
+    });
+  });
 });
